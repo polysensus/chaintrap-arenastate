363c770deee8ed8ab7c5c1043bfea9a278a9dd39
Robin Bryce <robinbryce@gmail.com>
2022-12-03 19:52:34 +0000

    player state tests

---
+++ src/lib/player.spec.js
@@ -0,0 +1,116 @@
+import { ethers } from "ethers";
+import { describe, it, expect } from "vitest";
+
+import { ABIName } from "./abiconst.js";
+import { Player } from "./player.js";
+// import msgpack from "@msgpack/msgpack";
+
+const arrayify = ethers.utils.arrayify;
+
+describe("Player", function () {
+  it("Should create empty player", async function () {
+    const p = new Player();
+    expect(p.address).to.equal(undefined);
+    // expect(ss._scene.a).to.equal(2);
+  });
+  it("Should set the registered state", async function () {
+    const p = new Player();
+    p.setState({ registered: true });
+    expect(p.state.registered).to.equal(true);
+  });
+
+  it("Should return correct snapshot", async function () {
+    const p = new Player();
+    p.setState({
+      registered: true,
+      address: "0x01",
+      location: arrayify("0x02"),
+      startLocation: arrayify("0x03"),
+      sceneblob: arrayify("0x04"),
+      halted: false,
+    });
+    const before = p.stateSnapshot();
+    expect(before.registered).toBe(true);
+    expect(before.address).toStrictEqual("0x01");
+    expect(before.location).toStrictEqual(arrayify("0x02"));
+    expect(before.startLocation).toStrictEqual(arrayify("0x03"));
+    expect(before.sceneblob).toStrictEqual(arrayify("0x04"));
+    expect(before.halted).toBe(false);
+  });
+
+  it("Should return empty delta", async function () {
+    const p = new Player();
+
+    const orig = {
+      registered: true,
+      address: "0x01",
+      location: arrayify("0x02"),
+      startLocation: arrayify("0x03"),
+      sceneblob: arrayify("0x04"),
+      halted: false,
+    };
+    p.setState(orig);
+
+    const delta = p.stateDelta(p.stateSnapshot(), p.stateSnapshot());
+    expect(delta.toObject()).toStrictEqual({});
+  });
+
+  it("Should return new location immediately", async function () {
+    const p = new Player();
+
+    const orig = {
+      registered: true,
+      address: "0x01",
+      location: arrayify("0x02"),
+      locationIngress: [1, 2],
+      startLocation: arrayify("0x03"),
+      sceneblob: arrayify("0x04"),
+      halted: false,
+    };
+    p.setState(orig);
+
+    const useExit = {
+      event: ABIName.UseExit,
+      transactionHash: "0xf1",
+      args: {
+        eid: 1,
+      },
+    };
+
+    const exitUsed = {
+      event: ABIName.ExitUsed,
+      transactionHash: "0xf2",
+      args: {
+        eid: 1, // ExitUsed is from the host, confirming the players declared UseExit. So it has the same transcript entry eid
+        outcome: {
+          location: arrayify("0x0202"),
+          sceneblob: arrayify("0x0404"),
+          side: 2,
+          ingressIndex: 1,
+        },
+      },
+    };
+
+    // Test that the paired events of UseExit (from the player) and ExitUsed
+    // (from the game host) produce the expected state delta.  Notice that the
+    // transcript entry id (eid) is the same for the player and the game host.
+    // The player declares the move and the game host responds.
+    let delta = p.addUseExit(useExit);
+    expect(delta.lastEID).toBe(1);
+    delete delta.lastEID;
+    expect(delta.pendingExitUsed).toBe(true);
+    delete delta.pendingExitUsed;
+    expect(delta.toObject()).toStrictEqual({});
+
+    delta = p.addExitUsed(exitUsed);
+    expect(delta.location).toStrictEqual(arrayify("0x0202"));
+    expect(delta.sceneblob).toStrictEqual(arrayify("0x0404"));
+    expect(delta.locationIngress).toStrictEqual([2, 1]);
+    delete delta.location;
+    delete delta.locationIngress;
+    delete delta.sceneblob;
+    expect(delta.pendingExitUsed).toBe(false);
+    delete delta.pendingExitUsed;
+    expect(delta.toObject()).toStrictEqual({});
+  });
+});
--- src/lib/playerstate.js
+++ src/lib/playerstate.js
@@ -1,131 +1,145 @@
-import { ethers } from 'ethers'
+import { ethers } from "ethers";
 
 export class PlayerState {
   // constructor () { }
 
-  toObject () {
-    const o = {}
-    if (typeof this.registered !== 'undefined') o.registered = this.registered
-    if (typeof this.address !== 'undefined') o.address = this.address
-    if (typeof this.profile !== 'undefined') o.profile = this.profile
-    if (typeof this.location !== 'undefined') o.location = this.location
-    if (typeof this.locationIngress !== 'undefined') o.locationIngress = this.locationIngress
-    if (typeof this.startLocation !== 'undefined') o.startLocation = this.startLocation
-    if (typeof this.sceneblob !== 'undefined') o.sceneblob = this.sceneblob
-    if (typeof this.halted !== 'undefined') o.halted = this.halted
-    if (typeof this.pendingExitUsed !== 'undefined') o.pendingExitUsed = this.pendingExitUsed
-    if (typeof this.lastEID !== 'undefined') o.lastEID = this.lastEID
-    return o
+  toObject() {
+    const o = {};
+    if (typeof this.registered !== "undefined") o.registered = this.registered;
+    if (typeof this.address !== "undefined") o.address = this.address;
+    if (typeof this.profile !== "undefined") o.profile = this.profile;
+    if (typeof this.location !== "undefined") o.location = this.location;
+    if (typeof this.locationIngress !== "undefined")
+      o.locationIngress = this.locationIngress;
+    if (typeof this.startLocation !== "undefined")
+      o.startLocation = this.startLocation;
+    if (typeof this.sceneblob !== "undefined") o.sceneblob = this.sceneblob;
+    if (typeof this.halted !== "undefined") o.halted = this.halted;
+    if (typeof this.pendingExitUsed !== "undefined")
+      o.pendingExitUsed = this.pendingExitUsed;
+    if (typeof this.lastEID !== "undefined") o.lastEID = this.lastEID;
+    return o;
   }
 
-  clone () {
-    const c = new PlayerState()
-    c.update(this.toObject())
-    return c
+  clone() {
+    const c = new PlayerState();
+    c.update(this.toObject());
+    return c;
   }
 
-  diff (other) {
-    const delta = {}
+  diff(other) {
+    const delta = {};
 
     if (this.registered !== other.registered) {
-      delta.registered = !!other.registered
+      delta.registered = !!other.registered;
     }
 
     if (this.address !== other.address) {
-      delta.address = other.address
+      delta.address = other.address;
     }
 
     if (this.profile !== other.profile) {
-      delta.profile = other.profile
+      delta.profile = other.profile;
     }
 
     if (this.halted !== other.halted) {
-      delta.halted = !!other.halted
+      delta.halted = !!other.halted;
     }
 
     if (this.pendingExitUsed !== other.pendingExitUsed) {
-      delta.pendingExitUsed = !!other.pendingExitUsed
+      delta.pendingExitUsed = !!other.pendingExitUsed;
     }
 
     if (this.startLocation !== other.startLocation) {
-      delta.startLocation = other.startLocation
+      delta.startLocation = other.startLocation;
     }
 
     if (this.location !== other.location) {
-      delta.location = other.location
+      delta.location = other.location;
     }
 
     if (this.locationIngress !== other.locationIngress) {
-      delta.locationIngress = other.locationIngress
+      delta.locationIngress = other.locationIngress;
     }
 
     if (this.sceneblob !== other.sceneblob) {
-      delta.sceneblob = other.sceneblob
-      delta.locationIngress = other.locationIngress
+      delta.sceneblob = other.sceneblob;
+      delta.locationIngress = other.locationIngress;
     }
 
     if (this.lastEID !== other.lastEID) {
-      delta.lastEID = other.lastEID
+      delta.lastEID = other.lastEID;
     }
-    return delta
+    return delta;
   }
 
   // @ts-ignore
-  update ({ registered, address, profile, location, locationIngress, startLocation, sceneblob, halted, pendingExitUsed, lastEID } = {}) {
-    if (typeof registered !== 'undefined') {
-      this.registered = registered
+  update({
+    registered,
+    address,
+    profile,
+    location,
+    locationIngress,
+    startLocation,
+    sceneblob,
+    halted,
+    pendingExitUsed,
+    lastEID,
+  } = {}) {
+    if (typeof registered !== "undefined") {
+      this.registered = registered;
     }
 
-    if (typeof address !== 'undefined') {
-      this.address = address
+    if (typeof address !== "undefined") {
+      this.address = address;
     }
 
     if (ethers.utils.isBytesLike(profile)) {
       if (ethers.utils.stripZeros(profile).length === 0) {
-        profile = undefined
+        profile = undefined;
       }
-      this.profile = profile
-    } else if (typeof profile !== 'undefined') {
-      this.profile = profile
+      this.profile = profile;
+    } else if (typeof profile !== "undefined") {
+      this.profile = profile;
     }
 
-    if (typeof location !== 'undefined') {
-      this.location = location
+    if (typeof location !== "undefined") {
+      this.location = location;
     }
 
-    if (typeof locationIngress !== 'undefined') {
-      this.locationIngress = locationIngress
+    if (typeof locationIngress !== "undefined") {
+      this.locationIngress = locationIngress;
     }
 
     if (ethers.utils.isBytesLike(startLocation)) {
       if (ethers.utils.stripZeros(startLocation).length === 0) {
-        startLocation = undefined
+        startLocation = undefined;
       }
     }
 
-    if (typeof startLocation !== 'undefined') {
-      this.startLocation = startLocation
+    if (typeof startLocation !== "undefined") {
+      this.startLocation = startLocation;
     }
 
-    if (typeof sceneblob !== 'undefined') {
-      this.sceneblob = sceneblob
+    if (typeof sceneblob !== "undefined") {
+      this.sceneblob = sceneblob;
     }
 
-    if (typeof halted !== 'undefined') {
-      this.halted = halted
+    if (typeof halted !== "undefined") {
+      this.halted = halted;
     }
 
-    if (typeof halted !== 'undefined') {
-      this.halted = halted
+    if (typeof halted !== "undefined") {
+      this.halted = halted;
     }
 
-    if (typeof pendingExitUsed !== 'undefined') {
-      this.pendingExitUsed = pendingExitUsed
+    if (typeof pendingExitUsed !== "undefined") {
+      this.pendingExitUsed = pendingExitUsed;
     }
 
-    if (typeof lastEID !== 'undefined') {
-      this.lastEID = lastEID
+    if (typeof lastEID !== "undefined") {
+      this.lastEID = lastEID;
     }
-    return this
+    return this;
   }
 }
