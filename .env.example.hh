57764cd0c833419621e4d81c1149c5a4b4e9d7ab
Robin Bryce <robin.bryce@jitsuin.com>
2023-07-30 14:32:33 +0100

    build: updates to work with diamond-deploy select and reduce the need for tusk

--- .env.example.hh
+++ .env.example.hh
@@ -2,6 +2,7 @@
 # pipeline secrets
 
 export ARENASTATE_LOGLEVEL=DEBUG
+export ARENASTATE_PROVIDER_URL="http://localhost:8545"
 
 # hardhat - in process, leaving the test setup to deploy the contracts
 # DONT export ARENASTATE_ARENA - absence is the switch to make it in process
